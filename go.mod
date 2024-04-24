5cf4ccc3a9a9e4bfbce75856b7c5ff430aa2c872
Jack Wickstrom <jwickstrom@unm.edu>
2024-04-23 20:06:57 -0600

    big refactor and add food map

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module pkg
+module myproject
 
 go 1.22.2
 
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"math/rand"
+	"myproject/physarum"
+	_ "net/http/pprof"
+)
+
+func main() {
+
+	//Set a static seed so we get the same results everytime
+	rand.Seed(42)
+
+	physarum.Run()
+}
